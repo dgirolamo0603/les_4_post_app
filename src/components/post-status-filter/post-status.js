cd558c1e1ca56f1f1ccaf6cd6141aa09b45d1064
Alex <dgirolamo777@gmail.com>
2019-06-18 15:08:28 +0300

    "all"

---
+++ src/components/post-status-filter/post-status.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './post-status-filter.css';
+
+const PostStatus=()=>{
+    return(
+        <div className="btn-group">
+            <button/>
+            <button/>
+        </div>
+    )
+}
+export default PostStatus;
